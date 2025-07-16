2ba31685a5ba602dca8f95b4996659e4bd1a726d
jobala <japhethobalak@gmail.com>
2025-07-16 16:03:23 +0300

    fix: remove disk manager cache

--- storage/disk/disk_manager.go
+++ storage/disk/disk_manager.go
@@ -7,28 +7,14 @@ import (
 
 func NewManager(file *os.File) *diskManager {
 	return &diskManager{
-		dbFile:       file,
-		pageCapacity: DEFAULT_PAGE_CAPACITY,
-		freeSlots:    []int64{},
-		pages:        map[int]int64{},
+		dbFile: file,
 	}
 }
 
 func (dm *diskManager) writePage(pageId int, data []byte) error {
-	var offset int64
-	offset, pageFound := dm.pages[pageId]
+	offset := int64(pageId * PAGE_SIZE)
 
-	if !pageFound {
-		offset, err := dm.allocatePage()
-		if err != nil {
-			return err
-		}
-		dm.pages[pageId] = offset
-	}
-
-	_, err := dm.dbFile.WriteAt(data, offset)
-
-	if err != nil {
+	if _, err := dm.dbFile.WriteAt(data, offset); err != nil {
 		return fmt.Errorf("error writing at offset %d: %v", offset, err)
 	}
 
@@ -36,16 +22,7 @@ func (dm *diskManager) writePage(pageId int, data []byte) error {
 }
 
 func (dm *diskManager) readPage(pageId int) ([]byte, error) {
-	var offset int64
-	offset, pageFound := dm.pages[pageId]
-
-	if !pageFound {
-		offset, err := dm.allocatePage()
-		if err != nil {
-			return nil, err
-		}
-		dm.pages[pageId] = offset
-	}
+	offset := int64(pageId * PAGE_SIZE)
 
 	buf := make([]byte, PAGE_SIZE)
 	if _, err := dm.dbFile.ReadAt(buf, offset); err != nil {
@@ -55,38 +32,6 @@ func (dm *diskManager) readPage(pageId int) ([]byte, error) {
 	return buf, nil
 }
 
-func (dm *diskManager) deletePage(pageId int) {
-	if offset, ok := dm.pages[pageId]; ok {
-		dm.freeSlots = append(dm.freeSlots, offset)
-		delete(dm.pages, pageId)
-	}
-}
-
-func (dm *diskManager) allocatePage() (int64, error) {
-	if len(dm.freeSlots) > 0 {
-		offset := dm.freeSlots[0]
-		dm.freeSlots = dm.freeSlots[1:]
-
-		return offset, nil
-	}
-
-	if len(dm.pages)+1 > dm.pageCapacity {
-		dm.pageCapacity *= 2
-		if err := os.Truncate(dm.dbFile.Name(), int64(dm.pageCapacity)*PAGE_SIZE); err != nil {
-			return -1, fmt.Errorf("error resizing db file: %v", err)
-		}
-	}
-
-	return dm.getNextOffset(), nil
-}
-
-func (dm *diskManager) getNextOffset() int64 {
-	return int64(len(dm.pages) * PAGE_SIZE)
-}
-
 type diskManager struct {
-	dbFile       *os.File
-	pages        map[int]int64
-	freeSlots    []int64
-	pageCapacity int
+	dbFile *os.File
 }
